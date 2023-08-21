3c9d2382438864255c0f3a6b0f00635927faad65
Akkshatt-19 <akshatp@shriffle.com>
2023-08-21 12:58:19 +0530

    Task Completed of Block And Follow

---
+++ app/models/block.rb
@@ -0,0 +1,7 @@
+class Block < ApplicationRecord
+    belongs_to :blocker, class_name: 'User'
+    belongs_to :blocked, class_name: 'User'
+
+    validates :blocker_id, uniqueness: { scope: :blocked_id }
+    validates :blocked_id, uniqueness: { scope: :blocker_id }
+end
